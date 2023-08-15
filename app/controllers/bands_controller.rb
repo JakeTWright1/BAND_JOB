0b3ab51e7a97918aa25924a0a84327bb0607a6fe
Jake Wright <jaketwright0@gmail.com>
2023-08-15 21:24:54 +0100

    Skeleton for Band Show page

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,8 +1,12 @@
 class BandsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
+  before_action :set_band, only: [:show]
 
   def index
     @bands = Band.all
+  end
+
+  def show
   end
 
   def new
@@ -22,5 +26,9 @@ class BandsController < ApplicationController
 
   def band_params
     params.require(:band).permit(:name, :genre, :price_per_night)
+  end
+
+  def set_band
+    @band = Band.find(params[:id])
   end
 end
