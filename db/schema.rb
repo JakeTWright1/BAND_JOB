adf9c07f255ac798d9e4b1e32eb2ada6bf28a50e
Jake Wright <jaketwright0@gmail.com>
2023-08-16 20:50:19 +0100

    added images to band seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_12_154853) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_16_194411) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_12_154853) do
     t.integer "price_per_night"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "users", force: :cascade do |t|
