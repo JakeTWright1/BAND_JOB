4ffbaad81dfa747991fbe6422a0269e124bf56c8
Jake Wright <jaketwright0@gmail.com>
2023-08-19 12:00:45 +0100

    created bookings show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "bands#home"
   resources :users do
-    resources :bookings, only: [:new, :create, :show]
+    resources :bookings, only: [:new, :create, :index]
   end
   resources :bands
 end
