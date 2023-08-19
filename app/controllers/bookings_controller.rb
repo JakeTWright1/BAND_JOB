c392abc70e3aebd08aec107c807b9ba1e7d2b2bb
Christian Roier <christianroier@hotmail.com>
2023-08-19 16:45:00 +0100

    add bookings functionality to app

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,11 +6,14 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
+    @booking.band = Band.find(params[:band_id])
     @booking.user = current_user
+    price = calculate_total_price(@booking.start_date, @booking.end_date, @booking.band.price_per_night)
+    @booking.total_price = price
     if @booking.save
-      redirect_to user_booking_path(current_user)
+      redirect_to user_bookings_path(current_user)
     else
-      render :new, status: :unprocessable_entity
+      render "bands/show"
     end
   end
 
@@ -18,5 +21,16 @@ class BookingsController < ApplicationController
 
   def booking_params
     params.require(:booking).permit(:total_price, :start_date, :end_date)
+  end
+end
+
+# calculation isn't quite working yet, needs to be looked at
+
+def calculate_total_price(start_date, end_date, price_per_night)
+  if start_date == end_date
+    @booking.band.price_per_night
+  else
+    number_of_nights = (end_date.to_date - start_date.to_date).to_i
+    number_of_nights >= 0 ? number_of_nights * price_per_night : 0
   end
 end
